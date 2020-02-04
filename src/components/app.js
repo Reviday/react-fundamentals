b9cb80cf05021eaee03321a24b21d20130a5434b
Seong-Kyu Yun <reviday@naver.com>
2020-02-04 11:38:11 +0900

    Commit Lecture 4-1
    [React.js] 강좌 4-1편: Contact 검색기능 구현 | input, sort, filter

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,10 @@
 import React from 'react';
+import Contact from './contact';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name : ''
-        };
-    }
     render() {
         return (
-            <div>
-                <button onClick={() => {this.setState({name: 'Reviday'})}}>Click</button>
-                <h1>Hello!!! {this.state.name}</h1>
-            </div>
+            <Contact/>        
         )
     }
 }
